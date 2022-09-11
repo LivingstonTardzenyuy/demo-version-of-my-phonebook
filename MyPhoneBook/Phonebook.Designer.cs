54641a201ff4ee472faeff73180c7eb5a629183a
livingstonkongnyuy <kongnyuylivingstone@gmail.com>
2022-09-11 03:38:23 +0100

    my demo version of my phone book

---
+++ MyPhoneBook/Phonebook.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace MyPhoneBook
+{
+    partial class Phonebook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelelte = new System.Windows.Forms.Button();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Label0 = new System.Windows.Forms.Label();
+            this.dgvPhoneBook = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPhoneBook)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(393, 24);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(178, 20);
+            this.txtSearch.TabIndex = 28;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(577, 22);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(79, 23);
+            this.btnSearch.TabIndex = 29;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Location = new System.Drawing.Point(110, 281);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(75, 23);
+            this.btnClearAll.TabIndex = 23;
+            this.btnClearAll.Text = "ClearAll";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(205, 281);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 25;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelelte
+            // 
+            this.btnDelelte.Location = new System.Drawing.Point(298, 279);
+            this.btnDelelte.Name = "btnDelelte";
+            this.btnDelelte.Size = new System.Drawing.Size(75, 23);
+            this.btnDelelte.TabIndex = 27;
+            this.btnDelelte.Text = "Delete";
+            this.btnDelelte.UseVisualStyleBackColor = true;
+            this.btnDelelte.Click += new System.EventHandler(this.btnDelelte_Click);
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(20, 89);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(362, 20);
+            this.txtTel.TabIndex = 16;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(20, 163);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(362, 20);
+            this.txtEmail.TabIndex = 17;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(20, 224);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(362, 20);
+            this.txtAddress.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Email";
+            // 
+            // Label0
+            // 
+            this.Label0.AutoSize = true;
+            this.Label0.Location = new System.Drawing.Point(17, 27);
+            this.Label0.Name = "Label0";
+            this.Label0.Size = new System.Drawing.Size(35, 13);
+            this.Label0.TabIndex = 21;
+            this.Label0.Text = "Name";
+            // 
+            // dgvPhoneBook
+            // 
+            this.dgvPhoneBook.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPhoneBook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPhoneBook.Location = new System.Drawing.Point(393, 51);
+            this.dgvPhoneBook.Name = "dgvPhoneBook";
+            this.dgvPhoneBook.ReadOnly = true;
+            this.dgvPhoneBook.RowTemplate.ReadOnly = true;
+            this.dgvPhoneBook.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPhoneBook.Size = new System.Drawing.Size(263, 251);
+            this.dgvPhoneBook.TabIndex = 20;
+            this.dgvPhoneBook.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPhoneBook_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Tel";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(20, 50);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(362, 20);
+            this.txtName.TabIndex = 15;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(20, 281);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 22;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Location = new System.Drawing.Point(119, 9);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(18, 13);
+            this.ID.TabIndex = 30;
+            this.ID.Text = "ID";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(143, 9);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(0, 13);
+            this.lblID.TabIndex = 31;
+            // 
+            // Phonebook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(693, 357);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelelte);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Label0);
+            this.Controls.Add(this.dgvPhoneBook);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnCreate);
+            this.MaximizeBox = false;
+            this.Name = "Phonebook";
+            this.Text = "Phonebook";
+            this.Load += new System.EventHandler(this.Phonebook_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPhoneBook)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelelte;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Label0;
+        private System.Windows.Forms.DataGridView dgvPhoneBook;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label lblID;
+    }
+}
---
+++ MyPhoneBook/Phonebook.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;                                                                     //helps us to have access to several tools for use in line 20 down
+namespace MyPhoneBook
+{
+    public partial class Phonebook : Form
+    {
+        public Phonebook()
+        {
+            InitializeComponent();
+        }
+        string conString = @"Data Source=.\SQLEXPRESS;Initial Catalog=PhoneBook1DB;Integrated Security=True";
+        SqlConnection con = new SqlConnection();                                                  //creating instances of class 
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        SqlDataReader dr;
+        DataTable dt = new DataTable();
+        private void Phonebook_Load(object sender, EventArgs e)
+        {
+            clearAll();                                                                 // naming a method clearall and using it down
+            LoadData();
+        }
+
+        void clearAll()
+        {
+            txtName.Clear();                                                            //making  sure by default the entries have no parameter
+            txtEmail.Clear();
+            txtAddress.Clear();
+            txtTel.Clear();
+            txtSearch.Clear();
+            lblID.Text = "";
+            txtName.Focus();
+
+            txtSearch.Clear();
+            // dgvPhoneBook.ClearSelection();
+            //makes sure that when our appliction start running the name textbox is always focus
+        }
+
+        object LoadData()
+        {
+
+            string query = "Select * From tblPhonebook1";                            //to load data to our data grid view
+            con = new SqlConnection(conString);                                              // creating a connection to our database 
+            con.Open();
+            cmd = new SqlCommand(query, con);
+            da = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);                                                                    //send all the infor from da into dt
+
+            if (dt.Rows.Count > 0)
+            {
+                return dgvPhoneBook.DataSource = dt;
+
+            }
+            else
+            {
+                dt = null;
+                return dt;
+            }
+
+        }
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "" || txtEmail.Text == "" || txtAddress.Text == "" || txtTel.Text == "")                  // this make sure users write all their information in the textbox
+            {
+                MessageBox.Show("Parameter missing", "status update", MessageBoxButtons.OK, MessageBoxIcon.Stop);    //here a box shows showing the missing parameter and the type of icon to show the error
+            }
+            else
+            {
+                string query = "Insert into tblPhoneBook1(Name,Tel,Email,Address) Values('" + txtName.Text + "','" + txtTel.Text + "','" + txtEmail.Text + "','" + txtAddress.Text + "')";            // allow users acces the dataBase using the insert query
+                con = new SqlConnection(conString);                                                  //sqlConnection string is used to take the conString to be use
+                con.Open();                                                                           // here we want to open the con 
+
+                cmd = new SqlCommand(query, con);                                                      // Sqlcommand takes two paramaters query and con
+
+                int i = cmd.ExecuteNonQuery();                                                      // manipulate the data, execute the data and store the result in the integer i
+
+                if (i > 0)                                                                            //chech if i is greater than 0 and if it is the case it means data has been entered into our system for processing and now we can copy our query section
+                {
+                    MessageBox.Show("Successful insertion", "status update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                else                                                                                // here i is not greater than 0
+                {
+                    MessageBox.Show("Error!", "status update", MessageBoxButtons.OK, MessageBoxIcon.Error);    //here a box shows showing the missing parameter and the type of icon to show the error            
+                }
+                clearAll();
+                LoadData();
+            }
+        }
+
+        private void btnDelelte_Click(object sender, EventArgs e)               // method to delete
+        {
+            {
+                if (lblID.Text == "")
+                {
+                    MessageBox.Show("Null Parameter", "status update", MessageBoxButtons.OK, MessageBoxIcon.Information);    //here a box shows showing the missing parameter and the type of icon to show the error
+                }
+                else
+                {
+                    string query = " Delete From tblPhoneBook1 where PhoneBook1ID='" + lblID.Text + "'";
+                    con = new SqlConnection(conString);                                                  //sqlConnection string is used to take the conString to be use
+                    con.Open();                                                                           // here we want to open the con 
+
+                    cmd = new SqlCommand(query, con);                                                      // Sqlcommand takes two paramaters query and con
+
+                    int i = cmd.ExecuteNonQuery();                                                      // manipulate the data, execute the data and store the result in the integer i
+
+                    if (i > 0)                                                                             //chech if i is greater than 0 and if it is the case it means data has been entered into our system for processing and now we can copy our query section
+                    {
+                        MessageBox.Show("Sucessful Delete", "status update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                    else                                                                                // here i is not greater than 0
+                    {
+                        MessageBox.Show("Failed Delet", "status update", MessageBoxButtons.OK, MessageBoxIcon.Error);    //here a box shows showing the missing parameter and the type of icon to show the error            
+                    }
+                    clearAll();                                                                                      // clear all the row of the data being deleted
+                    LoadData();                                                                                     // after our successful delition the dat shoudl be loaded
+                }
+
+            }
+        }
+
+        private void dgvPhoneBook_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            lblID.Text = dgvPhoneBook.SelectedRows[0].Cells[0].Value.ToString();
+            txtName.Text = dgvPhoneBook.SelectedRows[0].Cells[1].Value.ToString();
+            txtTel.Text = dgvPhoneBook.SelectedRows[0].Cells[2].Value.ToString();
+            txtEmail.Text = dgvPhoneBook.SelectedRows[0].Cells[3].Value.ToString();
+            txtAddress.Text = dgvPhoneBook.SelectedRows[0].Cells[4].Value.ToString();
+
+            //clearAll();
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            clearAll();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            {
+                {
+                    if (lblID.Text == "")
+                    {
+                        MessageBox.Show("Null Parameter", "status update", MessageBoxButtons.OK, MessageBoxIcon.Information);    //here a box shows showing the missing parameter and the type of icon to show the error
+                    }
+                    else
+                    {
+                        string query = "Update tblPhoneBook1 set Name='" + txtName.Text + "', Tel='" + txtTel.Text + "', Email='" + txtSearch.Text + "', Address='" + txtAddress.Text + "'where PhoneBook1ID='" + lblID.Text + "'";
+                        con = new SqlConnection(conString);                                                  //sqlConnection string is used to take the conString to be use
+                        con.Open();                                                                           // here we want to open the con 
+
+                        cmd = new SqlCommand(query, con);                                                      // Sqlcommand takes two paramaters query and con
+
+                        int i = cmd.ExecuteNonQuery();                                                      // manipulate the data, execute the data and store the result in the integer i
+
+                        if (i > 0)                                                                             //chech if i is greater than 0 and if it is the case it means data has been entered into our system for processing and now we can copy our query section
+                        {
+                            MessageBox.Show("Sucessful Update", "status update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+
+                        else                                                                                // here i is not greater than 0
+                        {
+                            MessageBox.Show("Failed Delet", "status update", MessageBoxButtons.OK, MessageBoxIcon.Error);    //here a box shows showing the missing parameter and the type of icon to show the error            
+                        }
+                        clearAll();                                                                                      // clear all the row of the data being deleted
+                        LoadData();                                                                                     // after our successful delition the dat shoudl be loaded
+                    }
+
+                }
+            }
+
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtSearch.Text=="")
+            {
+                MessageBox.Show("Null parameter!","status update", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+            }
+            else
+            {
+                 string query = "Select * From tblPhonebook1 where Name='"+ txtSearch.Text +"'";                            //to load data to our data grid view
+                 con = new SqlConnection(conString);                                              // creating a connection to our database 
+                 con.Open();
+                 cmd = new SqlCommand(query, con);
+                 da = new SqlDataAdapter(cmd);
+                 dt = new DataTable();
+                 da.Fill(dt);                                                                    //send all the infor from da into dt
+
+                if (dt.Rows.Count > 0)
+                {
+                    dgvPhoneBook.DataSource = dt;
+                    MessageBox.Show("Search Found", "status update", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                }
+                else
+                {
+                    LoadData();
+                    MessageBox.Show("Search not Found", "status update", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                }
+
+
+              
+
+            }
+         }
+    }
+}
+
